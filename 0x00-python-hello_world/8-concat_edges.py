52ef27c8c1eab7937e6c0dfcb5308c57f2e990f7
JoseR98 <josealonsoingeniero@hotmail.com>
2020-04-27 20:05:02 +0000

    Add new line

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -3,3 +3,4 @@ str = "Python is an interpreted, interactive, object-oriented programming\
         language that combines remarkable power with very clear syntax"
 str = str[39:67] + str[str.rfind("with"):str.rfind("with") + 5] + str[0:6]
 print(str)
+
