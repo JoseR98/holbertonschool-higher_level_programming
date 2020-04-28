0c344323ebe4e1ea8b1669d9d6d0f464f06e7abf
JoseR98 <josealonsoingeniero@hotmail.com>
2020-04-28 18:01:10 +0000

    Erase print function

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 for number in range(0, 100):
     if number != 99:
-        if number < 10:
-            print("0{:d}, ".format(number), end="")
-        else:
-            print("{:d}, ".format(number), end="")
+        print("{}{:d}".format("0" if number < 10 else "", number), end=", ")
     else:
         print("{:d}".format(number))
