9a8864e2c396b97d8e881031538ae70bf9cc33f6
JoseR98 <josealonsoingeniero@hotmail.com>
2020-04-28 17:37:43 +0000

    Add zero for 1 digit numbers

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 for number in range(0, 100):
     if number != 99:
-        print("{:d}, ".format(number), end="")
+        if number < 10:
+            print("0{:d}, ".format(number), end="")
+        else:
+            print("{:d}, ".format(number), end="")
     else:
         print("{:d}".format(number))
