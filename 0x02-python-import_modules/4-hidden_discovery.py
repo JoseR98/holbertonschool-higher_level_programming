945780a8e8ed784c2bf7af93e99add5191daae39
JoseR98 <josealonsoingeniero@hotmail.com>
2020-04-30 19:08:04 +0000

    Print names defined in a compiled module

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import hidden_4
+if __name__ == "__main__":
+    #dir function shows all the available attributes of the module
+    for n in dir(hidden_4):
+    # print only names that doesn´t start with '_'
+        if n[0] != '_':
+            print("{}".format(n))
