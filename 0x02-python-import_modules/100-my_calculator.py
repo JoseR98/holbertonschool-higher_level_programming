3ecf174ac823e76de4bb89c80b089cc3b3d4e977
JoseR98 <josealonsoingeniero@hotmail.com>
2020-04-30 21:07:03 +0000

    Handle basic operations fix 2

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,22 +2,24 @@
 from calculator_1 import add, sub, mul, div
 from sys import argv, exit
 num_args = len(argv)
-if num_args != 4:
-    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-    exit(1)
-if argv[2] == '+' or argv[2] == '-' or argv[2] == '*' or argv[2] == '/':
-    symbol = argv[2]
-    a = int(argv[1])
-    b = int(argv[3])
-    op = 0
-    if symbol == '+':
-        op = add(a, b)
-    elif symbol == '-':
-        op = sub(a, b)
-    elif symbol == '*':
-        op = mul(a, b)
-    elif symbol == '/':
-        op = div(a, b)
-    print("{:d} {} {:d} = {:d}".format(a, symbol, b, op))
-else:
-    print("Unknown operator. Available operators: +, -, * and /")
+if __name__ == "__main__":
+    if num_args != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    if argv[2] == '+' or argv[2] == '-' or argv[2] == '*' or argv[2] == '/':
+        symbol = argv[2]
+        a = int(argv[1])
+        b = int(argv[3])
+        op = 0
+        if symbol == '+':
+            op = add(a, b)
+        elif symbol == '-':
+            op = sub(a, b)
+        elif symbol == '*':
+            op = mul(a, b)
+        elif symbol == '/':
+            op = div(a, b)
+        print("{:d} {} {:d} = {:d}".format(a, symbol, b, op))
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+	exit(1)
