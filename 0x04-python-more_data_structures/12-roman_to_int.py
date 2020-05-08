8ba3333b177d4c692bdc4bf5d31e2959f4877226
JoseR98 <josealonsoingeniero@hotmail.com>
2020-05-08 00:40:06 +0000

    Try 2

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,7 +3,7 @@ def roman_to_int(roman_string):
     let = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     int_num = 0
     if type(roman_string) is str and roman_string:
-        l = list(let[i] for i in roman_string if i in let)
+        l = list(let[i] for i in roman_string)
         num_ones = 0
         for i in range(0, len(l)):
             if l[i] == 1:
