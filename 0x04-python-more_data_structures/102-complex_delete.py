6dde2054d5cb4cf268bfa7e8e88b537cd65769cd
JoseR98 <josealonsoingeniero@hotmail.com>
2020-05-08 02:17:23 +0000

    Delete key from dictionary

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,8 @@
+def complex_delete(a_dictionary, value):
+    new = {}
+    for key in a_dictionary.keys():
+        if a_dictionary[key] == value:
+            new[key] = a_dictionary[key]
+    for key in new.keys():
+        del(a_dictionary[key])
+    return a_dictionary
