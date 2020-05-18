2cd78b7ee2d9b8adb389a43c19150ae328467139
JoseR98 <josealonsoingeniero@hotmail.com>
2020-05-18 18:39:46 +0000

    Add safe print integer

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except ValueError as i:
+        print(i)
+        print("Exception: {}".format(i))
+        return False
+    except TypeError as j:
+        print("Exception: {}".format(j))
+        return False
