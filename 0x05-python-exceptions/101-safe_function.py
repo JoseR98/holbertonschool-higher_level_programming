c65bd20493c331dcd9e510eea96682d81d88a1a6
JoseR98 <josealonsoingeniero@hotmail.com>
2020-05-18 19:47:55 +0000

    Try fix outpu, case: change syntax and delete space

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -4,6 +4,6 @@ from sys import stderr
 
 def safe_function(fct, *args):
     try:
-        return (fct(*args))
+        return fct(*args)
     except Exception as i:
-        print("Exception: ", i, file=stderr)
+        print("Exception:", i, file=stderr)
