b0a19870d68b3e4788ef2b7b0cff20c5b3a2e003
JoseR98 <josealonsoingeniero@hotmail.com>
2020-05-18 19:00:28 +0000

    Delete exception, print on stderr

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
+from sys import stderr
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
     except ValueError as i:
-        print("Exception: {}".format(i))
+        print("Exception: {}".format(i), file=stderr)
         return False
     except TypeError as i:
-        print("Exception: {}".format(i))
-        return False
-    except NameError as i:
-        print("Exception: {}".format(i))
+        print("Exception: {}".format(i), file=stderr)
         return False
