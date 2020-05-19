595e210304fbb6cec5f4f1e566e3ade8cb904815
JoseR98 <josealonsoingeniero@hotmail.com>
2020-05-19 20:43:27 +0000

    Test style

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
+"""This module contains a class that defines a square.
+
+The name of the class is Square and it hasn´t attributes.
+"""
+
+
 class Square:
+    """Class that defines a square"""
     pass
