4597bc9e6cf3c664bca05607e43d77937b39f656
JoseR98 <josealonsoingeniero@hotmail.com>
2020-05-20 01:35:22 +0000

    Create MagicClass

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+from math import pi
+"""This module two operations for a circumference."""
+
+
+class MagicClass:
+    """Magic class that can determine area && circumference of a circle"""
+    def __init__(self, radius=0):
+        """__init__ method  """
+        self.__radius = 0
+        if type(self.__radius) is not int and type(self.__radius) is not float:
+            raise TypeError("radius must be a number")
+        self.__radius = radius
+
+    def area(self):
+        """area method that determines the area of the circle """
+        return ((self.__radius ** 2) * pi)
+
+    def circumference(self):
+        """circumference method determines the circumference of the circle """
+        return (self.__radius * 2 * pi)
