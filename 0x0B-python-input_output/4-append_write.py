3322d0a8a64910d3ac24da75ba45bc2fddcbaa2c
JoseR98 <josealonsoingeniero@hotmail.com>
2020-06-03 21:58:46 +0000

    Fix pep8

--- 0x0B-python-input_output/4-append_write.py
+++ 0x0B-python-input_output/4-append_write.py
@@ -3,7 +3,7 @@
 
 
 def append_write(filename="", text=""):
-    """append_write function that 
+    """append_write function that
 
     [extended_summary]
 
@@ -11,6 +11,6 @@ def append_write(filename="", text=""):
         filename (str, optional): [description]. Defaults to "".
         text (str, optional): [description]. Defaults to "".
     """
-    with open("{}".format(filename),"a",encoding="utf-8") as a_file:
+    with open("{}".format(filename), "a", encoding="utf-8") as a_file:
         num_char_add = a_file.write(text)
         return num_char_add
