d39caa0770cd48a8b71aa17585acced002f7c68f
JoseR98 <josealonsoingeniero@hotmail.com>
2020-06-03 22:41:13 +0000

    Add object to a json file

---
+++ 0x0B-python-input_output/9-add_item.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""This module add item(object) to a json file"""
+
+
+import sys
+import os.path
+load_from_json = __import__('8-load_from_json_file').load_from_json_file
+save_json = __import__('7-save_to_json_file').save_to_json_file
+
+list_1 = sys.argv[1:]
+list_readed = []
+if os.path.isfile("add_item.json"):
+    list_readed = load_from_json("add_item.json")
+    list_readed += list_1
+save_json(list_readed, "add_item.json")
