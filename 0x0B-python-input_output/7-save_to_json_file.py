7efe4737a0b8695f034cb3d82b7e3e4327bc01b6
JoseR98 <josealonsoingeniero@hotmail.com>
2020-06-03 22:56:11 +0000

    Fix

--- 0x0B-python-input_output/7-save_to_json_file.py
+++ 0x0B-python-input_output/7-save_to_json_file.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-""" """
+"""This module contains a function called save_to_json_file"""
+
 
 import json
 
 
 def save_to_json_file(my_obj, filename):
-    """save_to_json_file [summary]
+    """save_to_json_file function
 
     Args:
-        my_obj ([type]): [description]
-        filename ([type]): [description]
+        my_obj (object): python object
+        filename (str): string name
     """
-    with open('{}'.format(filename), 'w', encoding="utf-8") as file_a:
+    with open(filename, encoding="utf-8") as file_a:
         json_file = json.dumps(my_obj)
         file_a.write(json_file)
