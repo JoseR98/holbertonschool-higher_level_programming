425916d08c44467bd8ceeab5662e72bfb66c6eac
JoseR98 <josealonsoingeniero@hotmail.com>
2020-06-03 23:30:18 +0000

    Fix

--- 0x0B-python-input_output/8-load_from_json_file.py
+++ 0x0B-python-input_output/8-load_from_json_file.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-""" """
+"""This module contain a function called load_from_json:file"""
 
 import json
 
 
 def load_from_json_file(filename):
-    """load_from_json_file [summary]
+    """load_from_json_file function that load a json file
 
     Args:
-        filename ([type]): [description]
+        filename (json): json file
     """
     with open(filename, encoding="utf-8") as a_file:
-        return json.load(a_file)
+        return json.loads(a_file.read())
