b466a7cf7c3379af1ad5fd608f87a8f7cdf29fa3
JoseR98 <josealonsoingeniero@hotmail.com>
2020-06-03 22:50:10 +0000

    Fix

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -10,13 +10,13 @@ def read_lines(filename="", nb_lines=0):
         nb_lines (int, optional): lines to read from the file. Defaults to 0.
     """
     num_lines = 0
-    with open("{}".format(filename), encoding='utf-8') as a_file:
+    with open(filename, encoding='utf-8') as a_file:
         for i in a_file:
             num_lines += 1
-    with open("{}".format(filename), encoding='utf-8') as a_file:
+    with open(filename, encoding='utf-8') as a_file:
         if nb_lines <= 0 or nb_lines >= num_lines:
             print(a_file.read())
-    with open("{}".format(filename), encoding='utf-8') as a_file:
+    with open(filename, encoding='utf-8') as a_file:
         j = 0
         while j < nb_lines:
             print(a_file.readline(),end="")
