e83ab92914fbd990364722eb02074f8f198b022a
JoseR98 <josealonsoingeniero@hotmail.com>
2020-06-04 00:28:39 +0000

    Fix

--- 0x0B-python-input_output/10-class_to_json.py
+++ 0x0B-python-input_output/10-class_to_json.py
@@ -2,13 +2,10 @@
 """This module contains a function called def class_to_json"""
 
 
-import json
-
-
 def class_to_json(obj):
     """class_to_json function that return dict representation in json format
 
     Args:
         obj (class): is a class
     """
-    return json.dumps(obj.__dict__)
+    return obj.__dict__
