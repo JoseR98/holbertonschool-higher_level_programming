ea47b93d70494ec9a12b637269a10434b15c90b0
JoseR98 <josealonsoingeniero@hotmail.com>
2020-07-14 18:49:55 +0000

    Fix: add name of the database

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -2,4 +2,4 @@
 -- Creates the MySQL server user user_0d_1 and grants all privileges to it.
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
 ALTER USER 'user_0d_1'@'localhost' IDENTIFIED BY 'New-Password-Here';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON mysql.* TO 'user_0d_1'@'localhost';
