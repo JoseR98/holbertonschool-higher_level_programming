78cb0a3efc3d6309d3e70fb0439b17229ffe4ecd
JoseR98 <josealonsoingeniero@hotmail.com>
2020-07-15 01:19:55 +0000

    Fix: space

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,4 +1,4 @@
--- 16. List shows and genres 
+-- 16. List shows and genres
 -- Lists all shows, and all genres linked to that show, from the database.
 SELECT tv_shows.title, tv_genres.name
 FROM tv_shows LEFT JOIN tv_show_genres
