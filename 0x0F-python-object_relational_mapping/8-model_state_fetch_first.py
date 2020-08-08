41c047602e07131f736af14a0d2692109b4bbb93
JoseR98 <josealonsoingeniero@hotmail.com>
2020-08-08 14:53:40 +0000

    Add task from 0 to 13 + .gitignore

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Prints the first State object from the database hbtn_0e_6_usa"""
+
+if __name__ == "__main__":
+    import sys
+    from model_state import Base, State
+    from sqlalchemy.orm import sessionmaker
+    from sqlalchemy import (create_engine)
+    from sqlalchemy import exc
+
+    # Creates instance of the Database
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}\
+'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+
+    # Creates session factory bound to that engine
+    Session = sessionmaker(bind=engine)
+    # Instance of Session class
+    session = Session()
+    try:
+        record = session.query(State).order_by(State.id).first()
+        print("{}: {}".format(record.id, record.name))
+    except exc.SQLAlchemyError:
+        print("Nothing")
