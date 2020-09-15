fda79bbbfdf6bcaf2c6fd8907121403adc045e59
JoseR98 <josealonsoingeniero@hotmail.com>
2020-09-15 12:14:05 -0500

    Fix parsing to number using parseInt

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-if (!process.argv[2] || !Number(process.argv[2])) {
+if (!process.argv[2] || !parseInt(process.argv[2])) {
   console.log('No argument');
 } else {
-  console.log('My number: ' + Number(process.argv[2]));
+  console.log('My number: ' + parseInt(process.argv[2]));
 }
