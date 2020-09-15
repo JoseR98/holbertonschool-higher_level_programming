cc66371ce5bf37642790d2729bcb2f76e66187f6
JoseR98 <josealonsoingeniero@hotmail.com>
2020-09-15 12:15:55 -0500

    Fix validation of existence of first argument

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-if (!process.argv[2] || !parseInt(process.argv[2])) {
+if (!parseInt(process.argv[2])) {
   console.log('No argument');
 } else {
   console.log('My number: ' + parseInt(process.argv[2]));
