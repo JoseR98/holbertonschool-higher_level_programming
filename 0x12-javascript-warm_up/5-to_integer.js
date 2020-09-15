96af30dd6840245609b87df158b32678cc496cbc
JoseR98 <josealonsoingeniero@hotmail.com>
2020-09-15 12:17:44 -0500

    Fix output message. That was the original issue

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-if (!parseInt(process.argv[2])) {
-  console.log('No argument');
+if (!process.argv[2] || !Number(process.argv[2])) {
+  console.log('Not a number');
 } else {
-  console.log('My number: ' + parseInt(process.argv[2]));
+  console.log('My number: ' + Number(process.argv[2]));
 }
