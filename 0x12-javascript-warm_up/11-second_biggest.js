7a69a08a8e7b7f3762a089f6037e0a7e1f13feb6
JoseR98 <josealonsoingeniero@hotmail.com>
2020-09-15 13:37:47 -0500

    Fix not value condition

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,7 +1,5 @@
 #!/usr/bin/node
-if (!process.argv[2]) {
-  console.log(0);
-} else if (!process.argv[3]) {
+if (process.argv.length <= 3) {
   console.log(0);
 } else {
   const orderValues = process.argv.slice(2).sort();
