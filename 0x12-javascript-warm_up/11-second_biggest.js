38333150fccfb90da8a5c3c09fb0aa041a3c40e7
JoseR98 <josealonsoingeniero@hotmail.com>
2020-09-15 13:47:49 -0500

    Fix sort arguments, order by ascending order

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,6 +2,6 @@
 if (process.argv.length <= 3) {
   console.log(0);
 } else {
-  const orderValues = process.argv.slice(2).sort();
+  const orderValues = process.argv.slice(2).sort((a, b) => a - b);
   console.log(orderValues[orderValues.length - 2]);
 }
