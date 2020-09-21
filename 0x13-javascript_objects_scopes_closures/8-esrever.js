8b3fa453b3268e9a8b492b6c6354fd15eebda6a3
JoseR98 <josealonsoingeniero@hotmail.com>
2020-09-21 12:38:06 -0500

    Fix: missing one loop iteration

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -2,7 +2,7 @@
 // Reverse a given list
 exports.esrever = function (list) {
   const reverseList = [];
-  for (let index = 1; index < list.length; index++) {
+  for (let index = 1; index <= list.length; index++) {
     reverseList.push(list[list.length - index]);
   }
   return reverseList;
