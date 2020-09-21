20a6b5fafb7fc73b3d2fc414054f64e5c325df2f
JoseR98 <josealonsoingeniero@hotmail.com>
2020-09-21 12:27:29 -0500

    Fix: requiring wrong module

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,11 +1,8 @@
 #!/usr/bin/node
 // Class Square that inherits from Rectangle class
-module.exports = class Square extends required('./4-rectangle') {
-  constructor (size) {
-    super(size, size);
-  }
 
-  charPrint(c) {
+module.exports = class Square extends require('./5-square') {
+  charPrint (c) {
     for (let hei = 0; hei < this.height; hei++) {
       let result = '';
       for (let wid = 0; wid < this.width; wid++) {
