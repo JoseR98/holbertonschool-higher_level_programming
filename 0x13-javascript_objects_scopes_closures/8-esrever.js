d7abb8c38adbfff24c82bf6aa43770d0072e5994
JoseR98 <josealonsoingeniero@hotmail.com>
2020-09-21 12:36:45 -0500

    Fix: not using reverse built-in

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,5 +1,9 @@
 #!/usr/bin/node
 // Reverse a given list
 exports.esrever = function (list) {
-  return list.slice().reverse();
+  const reverseList = [];
+  for (let index = 1; index < list.length; index++) {
+    reverseList.push(list[list.length - index]);
+  }
+  return reverseList;
 };
