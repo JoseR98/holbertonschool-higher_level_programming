69b162eb9c0a0dc6855ac5f80ceec210979cec07
JoseR98 <josealonsoingeniero@hotmail.com>
2020-09-21 12:22:42 -0500

    Fix extends for square class

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,13 +1,11 @@
 #!/usr/bin/node
 // Class Square that inherits from Rectangle class
-const Rectangle = require('./4-rectangle');
-
-module.exports = class Square extends Rectangle {
+module.exports = class Square extends required('./4-rectangle') {
   constructor (size) {
     super(size, size);
   }
 
-  charPrint(c=undefined) {
+  charPrint(c) {
     for (let hei = 0; hei < this.height; hei++) {
       let result = '';
       for (let wid = 0; wid < this.width; wid++) {
