f05cb59020c9032372c6bf963e99c354ede97dc2
JoseR98 <josealonsoingeniero@hotmail.com>
2020-09-21 12:30:54 -0500

    Fix: using c paramter

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,14 +3,13 @@
 
 module.exports = class Square extends require('./5-square') {
   charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
     for (let hei = 0; hei < this.height; hei++) {
       let result = '';
       for (let wid = 0; wid < this.width; wid++) {
-        if (c) {
-          result += 'C';
-        } else {
-          result += 'X';
-        }
+        result += c;
       }
       console.log(result);
     }
