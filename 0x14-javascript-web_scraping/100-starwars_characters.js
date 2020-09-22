4ce4ad5682a5bd11ceb4febc7157f5f270fa4752
JoseR98 <josealonsoingeniero@hotmail.com>
2020-09-22 16:19:45 -0500

    Fix look for movieId in url attribute

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // Prints all characters of a Star Wars movie
 const request = require('request');
-const movieId = parseInt(process.argv[2]);
+const movieId = '/' + parseInt(process.argv[2]) + '/';
 const films = 'https://swapi-api.hbtn.io/api/films/';
 request(films, (error, response, body) => {
   if (error) {
@@ -9,7 +9,7 @@ request(films, (error, response, body) => {
   } else {
     const filmsList = JSON.parse(body).results;
     for (let film = 0; film < filmsList.length; film++) {
-      if (filmsList[film].episode_id === movieId) {
+      if (filmsList[film].url.includes(movieId)) {
         for (let character = 0; character < filmsList[film].characters.length; character++) {
           request(filmsList[film].characters[character], (error, response, body) => {
             if (error) {
