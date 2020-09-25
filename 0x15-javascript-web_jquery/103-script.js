f5df09e8d9c89164b28cdabb6b62a6577ce95671
JoseR98 <josealonsoingeniero@hotmail.com>
2020-09-24 20:24:01 -0500

    Add task 103

--- 0x15-javascript-web_jquery/103-script.js
+++ 0x15-javascript-web_jquery/103-script.js
@@ -1,1 +1,17 @@
+const url = 'https://fourtonfish.com/hellosalut/?lang=';
 
+$(document).ready(function () {
+  $('input#btn_translate').click(function () {
+    $.get(url + $('input#language_code').val(), function (info) {
+      $('div#hello').text(info.hello);
+    });
+  });
+
+  $('#language_code').keydown(function (e) {
+    if (e.keyCode === 13) {
+      $.get(url + $('input#language_code').val(), function (info) {
+        $('div#hello').text(info.hello);
+      });
+    }
+  });
+});
