5a3654842ee8d925784331b8b12ae77f5e832221
JoseR98 <josealonsoingeniero@hotmail.com>
2020-09-24 19:31:35 -0500

    Update task 102

--- 0x15-javascript-web_jquery/102-script.js
+++ 0x15-javascript-web_jquery/102-script.js
@@ -1,1 +1,8 @@
-
+$(document).ready(() => {
+  $('INPUT#btn_translate').click(() => {
+    const lang = $('INPUT#language_code').val();
+    $.get('https://www.fourtonfish.com/hellosalut/?lang=' + lang, (response) => {
+      $('div#hello').text(response.hello);
+    });
+  });
+});
